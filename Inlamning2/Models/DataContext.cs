be27b32fb5bd2905025690edf420cd7b053bd139
Emma Wibeck <Emmawibeck@gmail.com>
2022-11-16 12:30:22 +0100

    Add project files.

---
+++ Inlamning2/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inlamning2.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToContainer("Products").HasPartitionKey(x => x.Id);
+        }
+    }
+}
---
+++ Inlamning2/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Inlamning2.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime Created { get; set; } = DateTime.Now;
+
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ItemNumber { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
